8a1c33ea1d3474694b6578737e1b879960301111
dancunrandy <dancunrandy@gmail.com>
2023-03-27 08:12:21 +0300

    fixed errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-
+      protect_from_forgery with: :null_session
     rescue_from ActionController::UnknownFormat do
       render plain: "Invalid request format", status: :unsupported_media_type
     end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,58 +1,70 @@
 class PowersController < ApplicationController
-    before_action :set_power, only: [:show, :update]
-  
-    # GET /powers
-    def index
-      @powers = Power.all
+  before_action :set_power, only: [:show, :update]
+
+  # GET /powers
+  def index
+    @powers = Power.all
+
+    respond_to do |format|
+      format.json { render json: @powers.as_json(except: [:created_at, :updated_at]) }
+    end
+  end
+
+  # GET /powers/1
+  def show
+    if @power
       respond_to do |format|
-        format.json { render json: @powers }
+        format.json { render json: @power.as_json(except: [:created_at, :updated_at]) }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: { error: 'Power not found' }, status: :not_found }
       end
     end
+  end
   
-    # GET /powers/1
-    def show
+
+  # POST /powers
+  def create
+    @power = Power.new(power_params)
+
+    if @power.save
+      respond_to do |format|
+        format.json { render json: @power, status: :created }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @power.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /powers/1
+  def update
+    if @power.update(power_params)
       respond_to do |format|
         format.json { render json: @power }
       end
-    end
-  
-    # POST /powers
-    def create
-      @power = Power.new(power_params)
-  
-      if @power.save
-        respond_to do |format|
-          format.json { render json: @power, status: :created }
-        end
-      else
-        respond_to do |format|
-          format.json { render json: @power.errors, status: :unprocessable_entity }
-        end
+    else
+      respond_to do |format|
+        format.json { render json: @power.errors, status: :unprocessable_entity }
       end
     end
-  
-    # PATCH/PUT /powers/1
-    def update
-      if @power.update(power_params)
-        respond_to do |format|
-          format.json { render json: @power }
-        end
-      else
-        respond_to do |format|
-          format.json { render json: @power.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_power
-        @power = Power.find(params[:id])
-      end
-  
-      # Only allow a list of trusted parameters through.
-      def power_params
-        params.require(:power).permit(:name)
-      end
   end
-  
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_power
+      @power = Power.find_by(id: params[:id])
+      unless @power
+        respond_to do |format|
+          format.json { render json: { error: 'Power not found' }, status: :not_found }
+        end
+      end
+    end
+    
+    # Only allow a list of trusted parameters through.
+    def power_params
+      params.require(:power).permit(:name)
+    end
+end
